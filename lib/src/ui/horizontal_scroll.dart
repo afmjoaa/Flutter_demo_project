0bb5bf30a534b5cfb8922a74da695abb87f5a856
afmjoaa <afmjoaa@gmail.com>
2020-03-19 21:08:37 +0600

    better way to do things

--- lib/src/ui/horizontal_scroll.dart
+++ lib/src/ui/horizontal_scroll.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-
 class HorizontalScroll extends StatelessWidget {
   final myList = new List<int>.generate(15, (i) => i + 1);
 
@@ -26,48 +25,43 @@ class HorizontalScroll extends StatelessWidget {
                 image: DecorationImage(
                     image: AssetImage("images/wide_image.jpg"),
                     fit: BoxFit.cover)),
-            padding: EdgeInsets.symmetric(vertical:0, horizontal: 0),
             child: ListView.separated(
                 scrollDirection: Axis.horizontal,
                 separatorBuilder: (context, index) =>
                     SizedBox(width: MediaQuery.of(context).size.width / 150),
-                itemCount: myList.length,
-                itemBuilder: (context, index) => ClipRRect(
-                      child:Padding(
-                        padding: index == 0 ? EdgeInsets.fromLTRB(18, 14, 18, 14) : EdgeInsets.fromLTRB(2, 14, 18, 14),
+                itemCount: myList.length + 1,
+                itemBuilder: (context, index) => index == 0
+                    ? SizedBox(width: MediaQuery.of(context).size.width / 2.2)
+                    : ClipRRect(
+                        child: Padding(
+                        padding: EdgeInsets.fromLTRB(2, 14, 18, 14),
                         child: Container(
                           width: MediaQuery.of(context).size.width / 2.55,
                           height: 150,
                           child: Center(
-                              child: index == 0 ? null : Text(
-                                myList[index].toString(),
-                                style: TextStyle(
-                                    color: Colors.black,
-                                    fontSize: 24,
-                                    fontWeight: FontWeight.bold),
-                              )),
+                              child: Text(
+                            myList[index - 1].toString(),
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold),
+                          )),
                           decoration: BoxDecoration(
-                              color: index== 0? Colors.transparent : Colors.white,
-                              borderRadius: BorderRadius.all(Radius.circular(10)),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10)),
                               boxShadow: [
                                 BoxShadow(
-                                  color: index == 0 ? Colors.transparent : Colors.grey,
+                                  color: Colors.grey,
                                   offset: Offset(2.0, 2.0), //(x,y)
                                   blurRadius: 4.0,
                                 ),
                               ],
-                              /*gradient: index == 1 ?  LinearGradient(
+                              gradient: LinearGradient(
                                   colors: [Colors.blue[200], Colors.white],
                                   begin: Alignment.topLeft,
-                                  end: Alignment.bottomRight) :
-                              LinearGradient(
-                                  colors: [Colors.transparent, Colors.transparent],
-                                  begin: Alignment.topLeft,
-                                  end: Alignment.bottomRight)*/
-                          ),
+                                  end: Alignment.bottomRight)),
                         ),
-                      )
-                    )),
+                      ))),
           )
         ],
       ),
